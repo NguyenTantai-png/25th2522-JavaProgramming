e845dc21fb8f1b5d295839d56c076ca4d011733d
NguyenTantai-png <tai.nt.cc25tth@ntu.edu.vn>
2026-08-03 18:15:59 +0700

    Create xinchao.java

---
+++ helloworld/src/tiil/edu/xinchao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//
+		System.out.println("tôi lần đầu dùng Java, tôi đag học và thử nghiệm với GIT");
+
+	}
+
+}
